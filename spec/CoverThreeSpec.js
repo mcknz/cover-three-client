7be053fb2f6e1e64b342d6b20fe166cbc8c1d635
Jeff McKenzie <mail@mcknz.com>
2012-05-23 13:00:17 -0400

    added jasmine

---
+++ spec/CoverThreeSpec.js
@@ -0,0 +1,11 @@
+describe("App", function() {
+    it("runs", function() {
+      expect(2+2).toBe(4);
+    });
+});
+
+describe("CoverThreePage", function() {
+    it("starts with first player turn", function() {
+
+    });
+});
